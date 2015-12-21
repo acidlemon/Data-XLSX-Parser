1ce536f89a436ee903fe25a3c2789d5d01d43dff
Masatoshi Kawazoe (acidlemon) <acidlemon@beatsync.net>
2015-12-21 21:19:18 +0900

    Checking in changes prior to tagging of version 0.14.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d7ea9e2..e27c6ac 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for Perl extension Data-XLSX-Parser
    
     {{$NEXT}}
    
    +0.14 2015-12-21T12:19:11Z
    +
         - bug fix: revised implements for 0.13_01
         - notice:  Data::XLSX::Parser->sheet is now obsolete.

--- lib/Data/XLSX/Parser.pm
+++ lib/Data/XLSX/Parser.pm
@@ -2,7 +2,7 @@ package Data::XLSX::Parser;
 use strict;
 use warnings;
 
-our $VERSION = '0.13_01';
+our $VERSION = '0.14';
 
 use Data::XLSX::Parser::DocumentArchive;
 use Data::XLSX::Parser::Workbook;
